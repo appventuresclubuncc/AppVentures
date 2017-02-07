613534eaac3d8b78b527f8561e66cd2a43c80434
trvon <trvon@tuta.io>
2017-02-07 18:39:47 -0500

    WebView hosted by local htm file.

--- app/src/main/java/com/uncc/appventures/InfoActivity.java
+++ app/src/main/java/com/uncc/appventures/InfoActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.EditText;
 
 public class InfoActivity extends AppCompatActivity {
@@ -12,10 +13,14 @@ public class InfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.loadUrl("file:///android_asset/index.htm");
     }
 
     public void cabinetInfo(View view) {
         Intent intent = new Intent(this, CabinetActivity.class);
         startActivity(intent);
     }
+
 }

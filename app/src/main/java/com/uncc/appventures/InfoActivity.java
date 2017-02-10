b94877ab1d813ae24c8ba32f60196e0660c3be35
trvon <trvon@tuta.io>
2017-02-10 17:38:56 -0500

    Cleaning merge

--- app/src/main/java/com/uncc/appventures/InfoActivity.java
+++ app/src/main/java/com/uncc/appventures/InfoActivity.java
@@ -13,9 +13,6 @@ public class InfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
-
-        WebView myWebView = (WebView) findViewById(R.id.webview);
-        myWebView.loadUrl("file:///android_asset/index.htm");
     }
 
     public void cabinetInfo(View view) {

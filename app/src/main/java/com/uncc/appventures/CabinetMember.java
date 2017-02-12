79508639d43b7a657bca4a9ecb7b662e50de547d
Michael Jordan <Michael Jordan>
2017-02-12 17:40:29 -0500

    merge complete

--- app/src/main/java/com/uncc/appventures/CabinetMember.java
+++ app/src/main/java/com/uncc/appventures/CabinetMember.java
@@ -1,20 +1,57 @@
 package com.uncc.appventures;
 
 public class CabinetMember {
-    // Creates Memeber Object according to firebase fields
     private String name, position;
+    private String major, concentration;
+    private String reason, gradDate;
+    private int imageId;
 
-    public CabinetMember(String name, String position) {
+    public CabinetMember() {}
+
+    public CabinetMember(String name, String position, String major, String concentration, String reason, String gradDate) {
         this.name = name;
         this.position = position;
+        this.major = major;
+        this.concentration = concentration;
+        this.reason = reason;
+        this.gradDate = gradDate;
     }
 
-    public String getPosition() {
-        return this.position;
+    public CabinetMember(String name, String position, int imageId, String major, String concentration, String reason, String gradDate) {
+        this.name = name;
+        this.position = position;
+        this.imageId = imageId;
+        this.major = major;
+        this.concentration = concentration;
+        this.reason = reason;
+        this.gradDate = gradDate;
     }
 
     public String getName() {
-        return this.name;
+        return name;
     }
 
+    public String getPosition() {
+        return position;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public String getConcentration() {
+        return concentration;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public String getGradDate() {
+        return gradDate;
+    }
 }
